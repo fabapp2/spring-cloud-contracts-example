e865c739d407cf630e4cf4e0ecbcae9d6763fd57
fabapp2 <fkrueger@pivotal.io>
2020-02-18 16:37:31 +0100

    removed EXPLICIT testMode and configured base class to use RestAssuredMockMvc

--- provider-contracts-testing-git/producer/src/test/java/com/example/scc/producer/HelloServiceSccBase.java
+++ provider-contracts-testing-git/producer/src/test/java/com/example/scc/producer/HelloServiceSccBase.java
@@ -1,20 +1,26 @@
 package com.example.scc.producer;
 
 import io.restassured.RestAssured;
+import io.restassured.module.mockmvc.RestAssuredMockMvc;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @DirtiesContext
+@AutoConfigureMockMvc
 public class HelloServiceSccBase {
-    @LocalServerPort
-    int port;
+
+    @Autowired
+    MockMvc mockMvc;
 
     @BeforeEach
     public void setup() {
-        RestAssured.baseURI = "http://localhost:" + this.port;
+        RestAssuredMockMvc.mockMvc(mockMvc);
     }
 }
