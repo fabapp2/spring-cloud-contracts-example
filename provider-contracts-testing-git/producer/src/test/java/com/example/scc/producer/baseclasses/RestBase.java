e7c2b19ab3c4aefc7a2163a611982a899ff33c03
fabapp2 <fkrueger@pivotal.io>
2020-02-24 20:08:29 +0100

    added support for Spring Rest Docs

--- provider-contracts-testing-git/producer/src/test/java/com/example/scc/producer/baseclasses/RestBase.java
+++ provider-contracts-testing-git/producer/src/test/java/com/example/scc/producer/baseclasses/RestBase.java
@@ -1,23 +1,45 @@
 package com.example.scc.producer.baseclasses;
 
 import io.restassured.module.mockmvc.RestAssuredMockMvc;
+import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.TestInfo;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.mockmvc.MockMvcRestDocumentationConfigurer;
+import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 @DirtiesContext
-@AutoConfigureMockMvc
+@ExtendWith({RestDocumentationExtension.class, SpringExtension.class})
 public class RestBase {
 
+    private RequestSpecification spec;
+
     @Autowired
-    MockMvc mockMvc;
+    private WebApplicationContext context;
 
     @BeforeEach
-    public void setup() {
+    public void setup(TestInfo testInfo, RestDocumentationContextProvider restDocumentation) {
+       //  RestAssuredMockMvc.mockMvc(mockMvc)
+        final MockMvcRestDocumentationConfigurer configurer = documentationConfiguration(restDocumentation);
+        final RestDocumentationResultHandler document = document(getClass().getSimpleName() + "_" + testInfo.getTestMethod().get().getName());
+        MockMvc mockMvc = MockMvcBuilders.webAppContextSetup(this.context)
+                .apply(configurer)
+                .alwaysDo(document)
+                .build();
         RestAssuredMockMvc.mockMvc(mockMvc);
     }
+
 }
