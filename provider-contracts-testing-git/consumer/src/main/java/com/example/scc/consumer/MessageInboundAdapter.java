38521291971a8c2e6f34be3e3cbf5663d24f6872
fabapp2 <fkrueger@pivotal.io>
2020-02-24 12:12:39 +0100

    added message listener and test

--- provider-contracts-testing-git/consumer/src/main/java/com/example/scc/consumer/MessageInboundAdapter.java
+++ provider-contracts-testing-git/consumer/src/main/java/com/example/scc/consumer/MessageInboundAdapter.java
@@ -1,11 +1,22 @@
 package com.example.scc.consumer;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.config.BeanDefinitionHolder;
+import org.springframework.beans.factory.support.AbstractBeanDefinition;
+import org.springframework.beans.factory.support.BeanDefinitionBuilder;
+import org.springframework.beans.factory.support.BeanDefinitionReaderUtils;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.core.type.AnnotationMetadata;
 import org.springframework.messaging.Message;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
+import java.util.Map;
+
+@Component
 @EnableBinding(Sink.class)
 @RequiredArgsConstructor
 public class MessageInboundAdapter {
@@ -16,4 +27,5 @@ public class MessageInboundAdapter {
     public void onMessage(Message<String> message) {
         consumerOfHelloService.handleHelloMessage(message.getPayload());
     }
+
 }
--- provider-contracts-testing-git/consumer/src/test/java/com/example/scc/consumer/ConsumerApplicationTests.java
+++ provider-contracts-testing-git/consumer/src/test/java/com/example/scc/consumer/ConsumerApplicationTests.java
@@ -3,6 +3,7 @@ package com.example.scc.consumer;
 		import org.junit.jupiter.api.Test;
 		import org.springframework.beans.factory.annotation.Autowired;
 		import org.springframework.boot.test.context.SpringBootTest;
+		import org.springframework.cloud.contract.stubrunner.StubTrigger;
 		import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
 		import org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties;
 		import org.springframework.cloud.contract.verifier.messaging.boot.AutoConfigureMessageVerifier;
@@ -16,9 +17,18 @@ class ConsumerApplicationTests {
 	@Autowired
 	ConsumerOfHelloService consumerOfHelloService;
 
+	@Autowired
+	StubTrigger stubTrigger;
+
 	@Test
-	void contextLoads() {
+	public void useHelloService_should_call_hello_service_using_http() {
 		consumerOfHelloService.useHelloService("Hello");
+	}
+
+	@Test
+	public void inbound_message_should_call_service() {
+		stubTrigger.labels().forEach((k, v) -> System.out.println(k + " -> " + v));
+		stubTrigger.trigger("triggerHelloMessage");
 	}
 
 }
